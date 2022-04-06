f7b65db5c2e6e3a14732b40662e2780cbd987b9f
dschuck <chuck@developmentseed.org>
2022-04-06 22:08:54 +0000

    Adjust scripts for DPS

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 basedir=$(dirname "$(readlink -f "$0")")
 
-set -euo pipefail
-set -x
+set -xeuo pipefail
 
 mamba env create -f "${basedir}/environment.yml"
--- gedi-subset/subset.sh
+++ gedi-subset/subset.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
 
-if [[ ${CONDA_DEFAULT_ENV} != "gedi_subset" ]]; then
-    source activate gedi_subset
-fi
+set -xeuo pipefail
 
 basedir=$(dirname "$(readlink -f "$0")")
-
-set -euo pipefail
-set -x
+subset_py="conda run -n gedi_subset ${basedir}/subset.py"
 
 if test -d input; then
     # We are executing within a DPS job, so the AOI file was automatically
     # downloaded to the `input` directory.
     aoi=$(ls input/*)
-    ${basedir}/subset.py --verbose --aoi "${aoi}" --limit "${1:-10000}"
+    ${subset_py} --verbose --aoi "${aoi}" --limit "${1:-10000}"
 else
     # This was invoked directly, so simply pass all arguments through to the
     # Python script.
-    ${basedir}/subset.py "$@"
+    ${subset_py} "$@"
 fi
